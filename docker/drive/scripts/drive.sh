cae6d98be87ba22cf3f62dd13dc4a5f14722d440
Jerridan Quiring <jerridan@gmail.com>
2017-12-30 23:52:56 -0500

    Generates openvpn client certificate and key.

--- docker/drive/scripts/drive.sh
+++ docker/drive/scripts/drive.sh
@@ -8,6 +8,12 @@ generate_server_key() {
   echo "Server certificate and key pair created successfully."
 }
 
+generate_client_key() {
+  easyrsa --batch --req-cn=blink-client gen-req blink-client nopass
+  cp $EASYRSA_PKI/private/blink-client.key $OPENVPN/client/blink-client.key
+  echo "Client certificate and key pair created successfully."
+}
+
 generate_diffie_hellman_params() {
   openssl dhparam -out $OPENVPN/server/dh.pem 2048
   echo "Diffie Hellman parameters generated successfully."
@@ -25,6 +31,7 @@ generate_openvpn_server_config() {
 
 easyrsa init-pki
 generate_server_key
+generate_client_key
 generate_diffie_hellman_params
 generate_hmac_key
 generate_openvpn_server_config blink-drive.vpn.example
