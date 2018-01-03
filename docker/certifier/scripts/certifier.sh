aa45b219e86b4bd37bd1d5fd67d5944928bc0ac7
Jerridan Quiring <jerridan@gmail.com>
2018-01-02 23:15:09 -0500

    Moves all certificate and key generation to the certifier.

--- docker/certifier/scripts/certifier.sh
+++ docker/certifier/scripts/certifier.sh
@@ -2,10 +2,44 @@
 
 set -e
 
+generate_certificate_authority() {
+  easyrsa --batch --req-cn=blink-certifier build-ca nopass
+  cp $EASYRSA_PKI/ca.crt $BLINK/ca.crt
+  echo "Certificate Authority successfully created"
+}
+
+generate_server_key_and_certificate() {
+  easyrsa --batch --req-cn=$SERVERNAME gen-req $SERVERNAME nopass
+  easyrsa --batch --req-cn=$SERVERNAME sign-req server $SERVERNAME
+  cp $EASYRSA_PKI/private/$SERVERNAME.key $BLINK/$SERVERNAME.key
+  cp $EASYRSA_PKI/issued/$SERVERNAME.crt $BLINK/$SERVERNAME.crt
+  echo "Server certificate and key pair created successfully."
+}
+
+generate_client_key_and_certificate() {
+  easyrsa --batch --req-cn=$CLIENTNAME gen-req $CLIENTNAME nopass
+  easyrsa --batch --req-cn=$CLIENTNAME sign-req client $CLIENTNAME
+  cp $EASYRSA_PKI/private/$CLIENTNAME.key $BLINK/$CLIENTNAME.key
+  cp $EASYRSA_PKI/issued/$CLIENTNAME.crt $BLINK/$CLIENTNAME.crt
+  echo "Client certificate and key pair created successfully."
+}
+
+generate_diffie_hellman_params() {
+  openssl dhparam -out $BLINK/dh.pem 2048
+  echo "Diffie Hellman parameters generated successfully."
+}
+
+generate_hmac_key() {
+  openvpn --genkey --secret $BLINK/ta.key
+  echo "HMAC key generated successfully."
+}
+
 easyrsa init-pki
-easyrsa --batch --req-cn=blink-certifier build-ca nopass
+generate_certificate_authority
+generate_server_key_and_certificate
+generate_client_key_and_certificate
+generate_diffie_hellman_params
+generate_hmac_key
 
-cp $EASYRSA_PKI/ca.crt $BLINK/ca.crt
-
-echo "Certificate Authority successfully created"
-ls $BLINK/ca.crt
+echo "Certifier tasks complete"
+ls -la $BLINK
--- docker/drive/scripts/drive.sh
+++ docker/drive/scripts/drive.sh
@@ -2,28 +2,6 @@
 
 set -e
 
-generate_server_key() {
-  easyrsa --batch --req-cn=blink-drive gen-req blink-drive nopass
-  cp $EASYRSA_PKI/private/$SERVERNAME.key $OPENVPN/server/$SERVERNAME.key
-  echo "Server certificate and key pair created successfully."
-}
-
-generate_client_key() {
-  easyrsa --batch --req-cn=blink-client gen-req blink-client nopass
-  cp $EASYRSA_PKI/private/$CLIENTNAME.key $OPENVPN/client/$CLIENTNAME.key
-  echo "Client certificate and key pair created successfully."
-}
-
-generate_diffie_hellman_params() {
-  openssl dhparam -out $OPENVPN/server/dh.pem 2048
-  echo "Diffie Hellman parameters generated successfully."
-}
-
-generate_hmac_key() {
-  openvpn --genkey --secret $OPENVPN/server/ta.key
-  echo "HMAC key generated successfully."
-}
-
 generate_openvpn_server_config() {
   server_domain="$1"
   ovpn_generate_server_config.sh -d server_domain
@@ -34,9 +12,5 @@ generate_openvpn_client_config() {
 }
 
 easyrsa init-pki
-generate_server_key
-generate_client_key
-generate_diffie_hellman_params
-generate_hmac_key
 generate_openvpn_server_config blink-drive.vpn.example
 generate_openvpn_client_config
