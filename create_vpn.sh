f914845e74b54eec1f74931632cd33d21d47af9b
Jerridan Quiring <jerridan@gmail.com>
2018-03-03 21:36:49 -0500

    Removes old setup and configuration files.

--- create_vpn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-terraform apply
-vpn_ip="$(terraform output public_ip)"
-scp -oStrictHostKeyChecking=no -i ~/.ssh/terraform_rsa ubuntu@${vpn_ip}:client.ovpn ~/Desktop/
--- destroy_vpn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -f ~/Desktop/client.ovpn
-terraform destroy -force
