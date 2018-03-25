6441e6ca635c193b4a4db6870e6c8554582bbdd3
Jerridan Quiring <jerridan@gmail.com>
2018-03-25 18:50:12 -0400

    Renames all instances of 'BlinkVPN' to 'ShardVPN' in the certifier.

--- certifier/scripts/certifier.sh
+++ certifier/scripts/certifier.sh
@@ -3,34 +3,34 @@
 set -e
 
 generate_certificate_authority() {
-  easyrsa --batch --req-cn=blink-certifier build-ca nopass
-  cp ${EASYRSA_PKI}/ca.crt ${BLINK_KEYS}/ca.crt
+  easyrsa --batch --req-cn=shard-certifier build-ca nopass
+  cp ${EASYRSA_PKI}/ca.crt ${SHARD_VPN_KEYS}/ca.crt
   echo "Certificate Authority successfully created"
 }
 
 generate_server_key_and_certificate() {
   easyrsa --batch --req-cn=${SERVERNAME} gen-req ${SERVERNAME} nopass
   easyrsa --batch --req-cn=${SERVERNAME} sign-req server ${SERVERNAME}
-  cp ${EASYRSA_PKI}/private/${SERVERNAME}.key ${BLINK_KEYS}/${SERVERNAME}.key
-  cp ${EASYRSA_PKI}/issued/${SERVERNAME}.crt ${BLINK_KEYS}/${SERVERNAME}.crt
+  cp ${EASYRSA_PKI}/private/${SERVERNAME}.key ${SHARD_VPN_KEYS}/${SERVERNAME}.key
+  cp ${EASYRSA_PKI}/issued/${SERVERNAME}.crt ${SHARD_VPN_KEYS}/${SERVERNAME}.crt
   echo "Server certificate and key pair created successfully."
 }
 
 generate_client_key_and_certificate() {
   easyrsa --batch --req-cn=${CLIENTNAME} gen-req ${CLIENTNAME} nopass
   easyrsa --batch --req-cn=${CLIENTNAME} sign-req client ${CLIENTNAME}
-  cp ${EASYRSA_PKI}/private/${CLIENTNAME}.key ${BLINK_KEYS}/${CLIENTNAME}.key
-  cp ${EASYRSA_PKI}/issued/${CLIENTNAME}.crt ${BLINK_KEYS}/${CLIENTNAME}.crt
+  cp ${EASYRSA_PKI}/private/${CLIENTNAME}.key ${SHARD_VPN_KEYS}/${CLIENTNAME}.key
+  cp ${EASYRSA_PKI}/issued/${CLIENTNAME}.crt ${SHARD_VPN_KEYS}/${CLIENTNAME}.crt
   echo "Client certificate and key pair created successfully."
 }
 
 generate_diffie_hellman_params() {
-  openssl dhparam -out ${BLINK_KEYS}/dh.pem 2048
+  openssl dhparam -out ${SHARD_VPN_KEYS}/dh.pem 2048
   echo "Diffie Hellman parameters generated successfully."
 }
 
 generate_hmac_key() {
-  openvpn --genkey --secret ${BLINK_KEYS}/ta.key
+  openvpn --genkey --secret ${SHARD_VPN_KEYS}/ta.key
   echo "HMAC key generated successfully."
 }
 
