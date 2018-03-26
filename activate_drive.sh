c6e722eae20f8bbc40bc202f6d4b8c723259fd07
Jerridan Quiring <jerridan@gmail.com>
2018-03-25 23:34:02 -0400

    Adds flag to scripted terraform commands to include settings variables.

--- activate_drive.sh
+++ activate_drive.sh
@@ -5,6 +5,6 @@ set -e
 PROJECT_DIR=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 
 cd ${PROJECT_DIR}/drive
-terraform apply -auto-approve
+terraform apply -auto-approve -var-file=${PROJECT_DIR}/settings.tfvars
 
 echo "ShardVPN drive activated successfully"
--- generate_keys.sh
+++ generate_keys.sh
@@ -6,10 +6,10 @@ PROJECT_DIR=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 cd ${PROJECT_DIR}
 
 # Create S3 bucket for key storage
-terraform apply -auto-approve
+terraform apply -auto-approve -var-file=${PROJECT_DIR}/settings.tfvars
 
 # Generate the keys
 cd ${PROJECT_DIR}/certifier
-terraform apply -auto-approve
+terraform apply -auto-approve -var-file=${PROJECT_DIR}/settings.tfvars
 
 echo "ShardVPN keys successfully generated"
