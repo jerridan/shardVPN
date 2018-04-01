aaec8376e4defa5e5239cef85df036a7fe1a3acf
Jerridan Quiring <jerridan@gmail.com>
2018-03-31 23:31:32 -0400

    Adds flag to all destroy scripts to use settings file.

--- destroy_all_resources.sh
+++ destroy_all_resources.sh
@@ -4,12 +4,12 @@ set -e
 
 PROJECT_DIR=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 
-terraform destroy -force
+terraform destroy -force -var-file=${PROJECT_DIR}/settings.tfvars
 
 cd ${PROJECT_DIR}/certifier
-terraform destroy -force
+terraform destroy -force -var-file=${PROJECT_DIR}/settings.tfvars
 
 cd ${PROJECT_DIR}/drive
-terraform destroy -force
+terraform destroy -force -var-file=${PROJECT_DIR}/settings.tfvars
 
 echo "ShardVPN resources destroyed successfully"
--- destroy_certifier_resources.sh
+++ destroy_certifier_resources.sh
@@ -5,6 +5,6 @@ set -e
 PROJECT_DIR=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 
 cd ${PROJECT_DIR}/certifier
-terraform destroy -force
+terraform destroy -force -var-file=${PROJECT_DIR}/settings.tfvars
 
 echo "ShardVPN certifier destroyed successfully"
